12aea6d90d00da6578be3074fd140d6fa396bc11
Andriy <andsei91@gmail.com>
2019-12-15 19:13:38 +0100

    update: to 1.5

--- src/config/BatchConfig.ts
+++
@@ -1,9 +0,0 @@
-import * as env from 'env-var';
-import { config as envConfig } from 'dotenv';
-envConfig();
-
-export class BatchConfig {
-    public static readonly CRON_INTERVAL = env.get("CRON_INTERVAL", "*/5 * * * * *").asString();
-    public static readonly PARALLEL_NUMBER = env.get("PARALLEL_NUMBER", "5").asIntPositive();
-    public static readonly CRON_TIMEZONE = env.get("CRON_TIMEZONE", "Europe/Madrid").asString();
-}
--- src/config/FileManagerConfig.ts
+++
@@ -1,7 +0,0 @@
-import * as env from 'env-var';
-import { config as envConfig } from 'dotenv';
-envConfig();
-
-export class FileManagerConfig {
-    public static readonly HOST = env.get("FILE_MANAGER_HOST").asString();
-}
